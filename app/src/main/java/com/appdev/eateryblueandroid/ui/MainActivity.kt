8e564332adc6e408b325b50d4c186b7bf72f1cb8
Aastha Shah <aasthashah@aasthas-mbp-2.myfiosgateway.com>
2022-01-18 12:52:56 -0500

    Added the bottom nav bar back

--- app/src/main/java/com/appdev/eateryblueandroid/ui/MainActivity.kt
+++ app/src/main/java/com/appdev/eateryblueandroid/ui/MainActivity.kt
@@ -5,14 +5,15 @@ import android.os.Bundle
 import androidx.activity.compose.setContent
 import com.appdev.eateryblueandroid.ui.viewmodels.AllEateriesViewModel
 import com.appdev.eateryblueandroid.screens.home.SafeHomeScreen
+import com.appdev.eateryblueandroid.ui.components.MainScreen
 
 class MainActivity : AppCompatActivity() {
     val allEateriesViewModel = AllEateriesViewModel()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         setContent {
-            SafeHomeScreen(allEateriesViewModel)
+            //SafeHomeScreen(allEateriesViewModel)
+            MainScreen(context = this, allEateriesViewModel = allEateriesViewModel)
         }
     }
 }
