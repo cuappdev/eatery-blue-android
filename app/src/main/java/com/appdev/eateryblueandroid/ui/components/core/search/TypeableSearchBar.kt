e5be083978c7919fcf43ad2a8a73c2486f2bb3ac
Corfish123 <80220215+Corfish123@users.noreply.github.com>
2022-02-23 18:30:26 -0500

    added cancel button for search bar

--- app/src/main/java/com/appdev/eateryblueandroid/ui/components/core/search/TypeableSearchBar.kt
+++ app/src/main/java/com/appdev/eateryblueandroid/ui/components/core/search/TypeableSearchBar.kt
@@ -2,13 +2,12 @@ package com.appdev.eateryblueandroid.ui.components.core.search
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Icon
 import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
@@ -26,23 +25,39 @@ fun TypeableSearchBar(searchText : String) {
     ) {
         Row(
             modifier = Modifier
-                .fillMaxWidth()
-                .background(colorResource(id = R.color.gray00))
-                .padding(10.dp)
-        ) {
-            Icon(
-                painter = painterResource(id = R.drawable.ic_magnifying_glass),
-                tint = colorResource(id = R.color.gray05),
-                contentDescription = null,
-                modifier = Modifier.padding(top = 3.dp)
-            )
+                .fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ){
+            Row(
+                modifier = Modifier
+                    .background(colorResource(id = R.color.gray00))
+                    .padding(10.dp)
+                    .width(250.dp)
+
+            ) {
+                Icon(
+                    painter = painterResource(id = R.drawable.ic_magnifying_glass),
+                    tint = colorResource(id = R.color.gray05),
+                    contentDescription = null,
+                    modifier = Modifier.padding(top = 3.dp)
+                )
+                Text(
+                    text = searchText,
+                    textStyle = TextStyle.BODY_MEDIUM,
+                    color = colorResource(id = R.color.gray05),
+                    modifier = Modifier.padding(start = 5.dp)
+                )
+            }
             Text(
-                text = searchText,
+                text = "Cancel",
                 textStyle = TextStyle.BODY_MEDIUM,
-                color = colorResource(id = R.color.gray05),
-                modifier = Modifier.padding(start = 5.dp)
+                color = colorResource(id = R.color.black),
+                modifier = Modifier
+                    .padding(start = 12.dp)
+                    .align(Alignment.CenterVertically)
             )
         }
+
     }
 }
 
