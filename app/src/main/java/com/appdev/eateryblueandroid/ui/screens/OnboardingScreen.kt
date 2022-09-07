e0056bbca611c083f2bd55ebe278b09d61fa71aa
thisjustin123 <jjg283@cornell.edu>
2022-09-06 20:21:06 -0400

    Update build.gradle
    - Added dependency for ViewPagers
    - Updated required modules
    - compileSdk -> 32

--- app/src/main/java/com/appdev/eateryblueandroid/ui/screens/OnboardingScreen.kt
+++ app/src/main/java/com/appdev/eateryblueandroid/ui/screens/OnboardingScreen.kt
@@ -1,5 +1,6 @@
 package com.appdev.eateryblueandroid.ui.screens
 
+import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
@@ -22,96 +23,111 @@ import androidx.compose.ui.unit.dp
 import com.appdev.eateryblueandroid.R
 import com.appdev.eateryblueandroid.ui.components.core.Text
 import com.appdev.eateryblueandroid.ui.components.core.TextStyle
+import com.google.accompanist.pager.ExperimentalPagerApi
+import com.google.accompanist.pager.HorizontalPager
+import com.google.accompanist.pager.rememberPagerState
 
 @Composable
 fun OnboardingScreen() {
     val interactionSource = MutableInteractionSource()
-
-    Column(
-        modifier = Modifier
-            .fillMaxHeight()
-            .fillMaxWidth(),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
+    val opacity = animateFloatAsState(1f)
+    Box {
         Column(
             modifier = Modifier
-                .fillMaxHeight(.9f)
+                .fillMaxHeight()
                 .fillMaxWidth(),
             verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            Icon(
-                painter = painterResource(id = R.drawable.ic_eaterylogo_blue),
-                tint = colorResource(R.color.eateryBlue),
+            Column(
                 modifier = Modifier
-                    .width(96.dp)
-                    .height(96.dp),
-                contentDescription = null
-            )
-            Text(
-                text = "Eatery",
-                color = colorResource(id = R.color.eateryBlue),
-                textStyle = TextStyle.SUPER_TITLE
-            )
-            Surface(
-                shape = RoundedCornerShape(24.dp),
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(top = 24.dp, start = 48.dp, end = 48.dp),
-                elevation = 4.dp
+                    .fillMaxHeight(.9f)
+                    .fillMaxWidth(),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
             ) {
-                Row(
+                Icon(
+                    painter = painterResource(id = R.drawable.ic_eaterylogo_blue),
+                    tint = colorResource(R.color.eateryBlue),
                     modifier = Modifier
-                        .background(colorResource(id = R.color.white))
-                        .clickable(
-                            interactionSource = interactionSource,
-                            indication = rememberRipple()
-                        ) {
-
-                        },
-                    horizontalArrangement = Arrangement.Center,
-                    verticalAlignment = Alignment.CenterVertically
+                        .width(96.dp)
+                        .height(96.dp),
+                    contentDescription = null
+                )
+                Text(
+                    text = "Eatery",
+                    color = colorResource(id = R.color.eateryBlue),
+                    textStyle = TextStyle.SUPER_TITLE
+                )
+                Surface(
+                    shape = RoundedCornerShape(24.dp),
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(top = 24.dp, start = 48.dp, end = 48.dp),
+                    elevation = 4.dp
                 ) {
-                    Text(
-                        textStyle = TextStyle.HEADER_H4,
-                        text = "Get Started",
-                        modifier = Modifier.padding(top = 13.5.dp, bottom = 13.5.dp)
-                    )
+                    Row(
+                        modifier = Modifier
+                            .background(colorResource(id = R.color.white))
+                            .clickable(
+                                interactionSource = interactionSource,
+                                indication = rememberRipple()
+                            ) {
+
+                            },
+                        horizontalArrangement = Arrangement.Center,
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        Text(
+                            textStyle = TextStyle.HEADER_H4,
+                            text = "Get Started",
+                            modifier = Modifier.padding(top = 13.5.dp, bottom = 13.5.dp)
+                        )
+                    }
                 }
             }
-        }
-        Row(
-            modifier = Modifier.fillMaxWidth(),
-            horizontalArrangement = Arrangement.Center,
-            verticalAlignment = Alignment.CenterVertically
-        ) {
-            Icon(
-                painter = painterResource(id = R.drawable.ic_appdev),
-                modifier = Modifier.padding(end = 6.dp),
-                contentDescription = null,
-                tint = colorResource(id = R.color.gray03)
-            )
-            Text(
-                buildAnnotatedString {
-                    withStyle(
-                        style = SpanStyle(
-                            color = colorResource(R.color.gray03),
-                            fontWeight = FontWeight.Normal
-                        )
-                    ) {
-                        append("Cornell")
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.Center,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Icon(
+                    painter = painterResource(id = R.drawable.ic_appdev),
+                    modifier = Modifier.padding(end = 6.dp),
+                    contentDescription = null,
+                    tint = colorResource(id = R.color.gray03)
+                )
+                Text(
+                    buildAnnotatedString {
+                        withStyle(
+                            style = SpanStyle(
+                                color = colorResource(R.color.gray03),
+                                fontWeight = FontWeight.Normal
+                            )
+                        ) {
+                            append("Cornell")
+                        }
+                        withStyle(
+                            style = SpanStyle(
+                                color = colorResource(R.color.gray03),
+                                fontWeight = FontWeight.SemiBold
+                            )
+                        ) {
+                            append("AppDev")
+                        }
                     }
-                    withStyle(
-                        style = SpanStyle(
-                            color = colorResource(R.color.gray03),
-                            fontWeight = FontWeight.SemiBold
-                        )
-                    ) {
-                        append("AppDev")
-                    }
-                }
-            )
+                )
+            }
         }
+    }
+}
+
+@OptIn(ExperimentalPagerApi::class)
+@Composable
+fun OnboardingViewPager() {
+    HorizontalPager(count = 3) { page ->
+        Text(
+            text = "Placeholder Text $page"
+        )
     }
 }
