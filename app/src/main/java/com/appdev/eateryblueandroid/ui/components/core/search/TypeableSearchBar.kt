9e3b976fead69104661eb3a46f13bb2d64ba25f0
thisjustin123 <jjg283@cornell.edu>
2022-11-11 00:41:30 -0500

    Fix search bar padding

--- app/src/main/java/com/appdev/eateryblueandroid/ui/components/core/search/TypeableSearchBar.kt
+++ app/src/main/java/com/appdev/eateryblueandroid/ui/components/core/search/TypeableSearchBar.kt
@@ -1,25 +1,20 @@
 package com.appdev.eateryblueandroid.ui.components.core.search
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material.Icon
-import androidx.compose.material.Surface
-import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.platform.LocalFocusManager
-import androidx.compose.ui.platform.LocalLifecycleOwner
-import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.input.ImeAction
-import androidx.compose.ui.text.input.KeyboardType
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.appdev.eateryblueandroid.R
 import com.appdev.eateryblueandroid.ui.components.core.Text
@@ -45,8 +40,7 @@ fun TypeableSearchBar(
                 onValueChange = { searchViewModel.onTextChange(it) },
                 modifier = Modifier
                     .focusRequester(focusRequester)
-                    .fillMaxWidth()
-                    .padding(12.dp),
+                    .fillMaxWidth(),
                 placeholder = "Search for grub...",
                 backgroundColor = colorResource(id = R.color.gray00),
                 focusRequester = focusRequester,
