a2ac447ca06ecf3030b6480ba01201eab906e6ff
Sophie Meng <sym28@cornell.edu>
2023-09-27 18:28:23 -0400

    Fix bugs UpcomingMenuScreen and release gradle issues

--- app/src/main/java/com/cornellappdev/android/eateryblue/ui/screens/UpcomingMenuScreen.kt
+++ app/src/main/java/com/cornellappdev/android/eateryblue/ui/screens/UpcomingMenuScreen.kt
@@ -6,13 +6,36 @@ import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.*
-import androidx.compose.runtime.*
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.ModalBottomSheetLayout
+import androidx.compose.material.ModalBottomSheetValue
+import androidx.compose.material.Text
+import androidx.compose.material.TextButton
+import androidx.compose.material.rememberModalBottomSheetState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -60,6 +83,7 @@ fun UpcomingMenuScreen(
     /** instantiates the meal filter list*/
 
     val selectedMealFilters = remember { mutableStateListOf<Filter>() }
+    if (selectedMealFilters.isEmpty()) selectedMealFilters.add(Filter.BREAKFAST)
     if (modalBottomSheetState.currentValue != ModalBottomSheetValue.Hidden) {
         DisposableEffect(Unit) {
             onDispose {
@@ -161,7 +185,7 @@ fun UpcomingMenuScreen(
                                             modifier = Modifier.align(Alignment.Center),
                                             textAlign = TextAlign.Center,
                                             text = "Upcoming Menus",
-                                            color = Color.White,
+                                            color = White,
                                             style = TextStyle(
                                                 fontWeight = FontWeight.SemiBold,
                                                 fontSize = 20.sp
@@ -178,7 +202,7 @@ fun UpcomingMenuScreen(
                                     ) {
                                         Text(
                                             text = "Upcoming Menus",
-                                            color = Color.White,
+                                            color = White,
                                             style = EateryBlueTypography.h2
                                         )
                                     }
