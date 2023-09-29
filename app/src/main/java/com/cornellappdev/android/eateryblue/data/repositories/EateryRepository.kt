7066b41421e072626b70247c4d38d51893dc27b6
thisjustin123 <jjg283@cornell.edu>
2023-09-29 00:24:46 -0400

    Document code in `EateryRepository`

--- app/src/main/java/com/cornellappdev/android/eateryblue/data/repositories/EateryRepository.kt
+++ app/src/main/java/com/cornellappdev/android/eateryblue/data/repositories/EateryRepository.kt
@@ -27,17 +27,22 @@ class EateryRepository @Inject constructor(private val networkApi: NetworkApi) {
     private val _eateryFlow: MutableStateFlow<EateryApiResponse<List<Eatery>>> =
         MutableStateFlow(EateryApiResponse.Pending)
 
+    /**
+     * A [StateFlow] emitting [EateryApiResponse]s for lists of ALL eateries, if loaded successfully.
+     */
     val eateryFlow = _eateryFlow.asStateFlow()
 
     init {
+        // Start loading backend as soon as the app initializes.
         pingBackend()
     }
 
     /**
      * Makes a new call to backend for all the eatery data.
-     * TODO: Add some way of letting a user re-ping by calling this method when an error occurs.
      */
     fun pingBackend() {
+        // TODO: Add some way of letting a user re-ping by calling this method when an error occurs.
+        //  There should be some sort of UI (e.g. pull up to refresh, error state w/ button, etc.)
         CoroutineScope(Dispatchers.Main).launch {
             try {
                 val eateries = getAllEateries()
