3e5c2ee2a125f72e3e384cb3f41869de10178fd6
thisjustin123 <jjg283@cornell.edu>
2023-10-05 12:36:28 -0400

    Change `HomeViewModel` and `HomeScreen` to Flows

--- app/src/main/java/com/cornellappdev/android/eateryblue/ui/viewmodels/HomeViewModel.kt
+++ app/src/main/java/com/cornellappdev/android/eateryblue/ui/viewmodels/HomeViewModel.kt
@@ -10,12 +10,17 @@ import com.cornellappdev.android.eateryblue.data.models.Eatery
 import com.cornellappdev.android.eateryblue.data.repositories.EateryRepository
 import com.cornellappdev.android.eateryblue.data.repositories.UserPreferencesRepository
 import com.cornellappdev.android.eateryblue.ui.components.general.Filter
-import com.cornellappdev.android.eateryblue.ui.viewmodels.state.EateryRetrievalState
+import com.cornellappdev.android.eateryblue.ui.viewmodels.state.EateryApiResponse
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
-import kotlinx.coroutines.sync.Mutex
-import kotlinx.coroutines.sync.withLock
 import javax.inject.Inject
 
 @HiltViewModel
@@ -23,25 +28,36 @@ class HomeViewModel @Inject constructor(
     private val userPreferencesRepository: UserPreferencesRepository,
     private val eateryRepository: EateryRepository
 ) : ViewModel() {
-    var eateryRetrievalState: EateryRetrievalState by mutableStateOf(EateryRetrievalState.Pending)
-        private set
+    private val _filtersFlow: MutableStateFlow<List<Filter>> = MutableStateFlow(listOf())
+    val filtersFlow = _filtersFlow.asStateFlow()
 
-    private val _currentFiltersSelected = mutableStateListOf<Filter>()
-    val currentFiltersSelected: List<Filter> = _currentFiltersSelected
+    val eateryFlow: StateFlow<EateryApiResponse<List<Eatery>>> =
+        eateryRepository.homeEateryFlow.combine(_filtersFlow) { apiResponse, filters ->
+            when (apiResponse) {
+                is EateryApiResponse.Error -> EateryApiResponse.Error
+                is EateryApiResponse.Pending -> EateryApiResponse.Pending
+                is EateryApiResponse.Success -> {
+                    EateryApiResponse.Success(
+                        apiResponse.data.filter {
+                            passesFilter(it, filters)
+                        })
+                }
+            }
+        }.stateIn(viewModelScope, SharingStarted.Eagerly, EateryApiResponse.Pending)
 
-    private val _allEateries = mutableSetOf<Eatery>()
-    val allEateries: Set<Eatery> = _allEateries
-
-    private val mutex = Mutex()
-
+    // TODO: Favorites
     var favoriteEateries = mutableStateListOf<Eatery>()
         private set
 
-    var nearestEateries = mutableStateListOf<Eatery>()
-        private set
-
-    var filteredResults = mutableStateListOf<Eatery>()
-        private set
+    var nearestEateries: StateFlow<List<Eatery>> = eateryFlow.map { apiResponse ->
+        when (apiResponse) {
+            is EateryApiResponse.Error -> listOf()
+            is EateryApiResponse.Pending -> listOf()
+            is EateryApiResponse.Success -> {
+                apiResponse.data.sortedBy { it.getWalkTimes() }.slice(0..5)
+            }
+        }
+    }.stateIn(viewModelScope, SharingStarted.Eagerly, listOf())
 
     private var bigPopUp by mutableStateOf(false)
 
@@ -49,133 +65,76 @@ class HomeViewModel @Inject constructor(
         bigPopUp = bool
     }
 
-    init {
-        queryAllEateries()
-    }
-
-    // TODO: Change to directly read from [EateryRepository]'s `homeEateryFlow`
-    //  Also, combine with the favorites map flow.
-    fun queryAllEateries() = viewModelScope.launch {
-        try {
-            val eateryResponse = eateryRepository.getHomeEateries()
-            _allEateries.addAll(eateryResponse)
-
-            val favoriteEateriesIds =
-                userPreferencesRepository.getFavoritesMap().keys
-            favoriteEateries.addAll(_allEateries.filter {
-                favoriteEateriesIds.contains(it.id)
-            })
-
-            eateryRetrievalState = EateryRetrievalState.Success
-        } catch (e: Exception) {
-            eateryRetrievalState = EateryRetrievalState.Error
-        }
-    }
-
-    fun updateFavorites() = viewModelScope.launch {
-        mutex.withLock {
-            val favoriteEateriesKeys =
-                userPreferencesRepository.getFavoritesMap().keys
-            favoriteEateries = _allEateries.filter {
-                favoriteEateriesKeys.contains(it.id)
-            }.toCollection(mutableStateListOf())
-        }
-    }
-
-    fun updateNearest() = viewModelScope.launch {
-        var eateries = _allEateries.sortedBy { it.getWalkTimes() }
-        nearestEateries = eateries.slice(0..5).toCollection(
-            mutableStateListOf()
-        )
-    }
-
     fun addFilter(filter: Filter) = viewModelScope.launch {
-        _currentFiltersSelected.add(filter)
-        filterEateries()
+        val newList = _filtersFlow.value.toMutableList()
+        newList.add(filter)
+        _filtersFlow.value = newList
     }
 
     fun removeFilter(filter: Filter) = viewModelScope.launch {
-        _currentFiltersSelected.remove(filter)
-        filterEateries()
+        val newList = _filtersFlow.value.toMutableList()
+        newList.remove(filter)
+        _filtersFlow.value = newList
     }
 
     fun addPaymentMethodFilters(filters: List<Filter>) = viewModelScope.launch {
-        _currentFiltersSelected.removeAll(Filter.PAYMENT_METHODS)
-        _currentFiltersSelected.addAll(filters)
-        filterEateries()
+        val newList = _filtersFlow.value.toMutableList()
+        newList.removeAll(Filter.PAYMENT_METHODS)
+        newList.addAll(filters)
+        _filtersFlow.value = newList
     }
 
     fun resetFilters() = viewModelScope.launch {
-        _currentFiltersSelected.clear()
-    }
-
-    private fun filterEateries() = viewModelScope.launch {
-        filteredResults = _allEateries.filter { eatery ->
-            passesFilter(eatery)
-        }.toCollection(mutableStateListOf())
+        _filtersFlow.value = listOf()
     }
 
     /**
      * Determines if the eatery passes the filter inspection based on what's currently selected.
      */
-    private fun passesFilter(eatery: Eatery): Boolean {
+    private fun passesFilter(eatery: Eatery, filters: List<Filter>): Boolean {
         var passesFilter = true
-        if (_currentFiltersSelected.contains(Filter.UNDER_10)) {
+        if (filters.contains(Filter.UNDER_10)) {
             val walkTimes = eatery.getWalkTimes()
             passesFilter = walkTimes != null && walkTimes <= 10
         }
 
-        if (_currentFiltersSelected.contains(Filter.FAVORITES)) {
+        if (filters.contains(Filter.FAVORITES)) {
             passesFilter = favoriteEateries.any {
                 it.id == eatery.id
             }
         }
 
         val allLocationsValid =
-            !_currentFiltersSelected.contains(Filter.NORTH) &&
-                !_currentFiltersSelected.contains(Filter.CENTRAL) &&
-                !_currentFiltersSelected.contains(Filter.WEST)
+            !filters.contains(Filter.NORTH) &&
+                    !filters.contains(Filter.CENTRAL) &&
+                    !filters.contains(Filter.WEST)
 
         // Passes filter if all locations aren't selected (therefore any location is valid, specified by allLocationsValid)
         // or one/multiple are selected and the eatery is located there.
         passesFilter = passesFilter &&
-            (allLocationsValid || ((_currentFiltersSelected.contains(Filter.NORTH) && eatery.campusArea == "North") ||
-                (_currentFiltersSelected.contains(Filter.WEST) && eatery.campusArea == "West") ||
-                (_currentFiltersSelected.contains(Filter.CENTRAL) && eatery.campusArea == "Central")))
+                (allLocationsValid || ((filters.contains(Filter.NORTH) && eatery.campusArea == "North") ||
+                        (filters.contains(Filter.WEST) && eatery.campusArea == "West") ||
+                        (filters.contains(Filter.CENTRAL) && eatery.campusArea == "Central")))
 
         val allPaymentMethodsValid =
-            !_currentFiltersSelected.contains(Filter.CASH) &&
-                !_currentFiltersSelected.contains(Filter.BRB) &&
-                !_currentFiltersSelected.contains(Filter.SWIPES)
+            !filters.contains(Filter.CASH) &&
+                    !filters.contains(Filter.BRB) &&
+                    !filters.contains(Filter.SWIPES)
 
         // Passes filter if all three aren't selected (therefore any payment method is valid, specified by allPaymentMethodsValid)
         // or one/multiple are selected and the eatery takes it.
         return passesFilter &&
-            (allPaymentMethodsValid || ((_currentFiltersSelected.contains(Filter.SWIPES) && eatery.paymentAcceptsMealSwipes == true) ||
-                (_currentFiltersSelected.contains(Filter.BRB) && eatery.paymentAcceptsBrbs == true) ||
-                (_currentFiltersSelected.contains(Filter.CASH) && eatery.paymentAcceptsCash == true)))
+                (allPaymentMethodsValid || ((filters.contains(Filter.SWIPES) && eatery.paymentAcceptsMealSwipes == true) ||
+                        (filters.contains(Filter.BRB) && eatery.paymentAcceptsBrbs == true) ||
+                        (filters.contains(Filter.CASH) && eatery.paymentAcceptsCash == true)))
     }
 
-    // Mutex needed to address different coroutine access
     fun addFavorite(eateryId: Int?) = viewModelScope.launch {
-        if (eateryId == null) return@launch
-        mutex.withLock {
-            userPreferencesRepository.setFavorite(eateryId, true)
-            _allEateries.firstOrNull { eatery ->
-                eatery.id == eateryId
-            }?.let { matchingEatery -> favoriteEateries.add(matchingEatery) }
-        }
+        // TODO: Fix favoriting flow.
     }
 
     fun removeFavorite(eateryId: Int?) = viewModelScope.launch {
-        if (eateryId == null) return@launch
-
-        mutex.withLock {
-            userPreferencesRepository.setFavorite(eateryId, false)
-            favoriteEateries.removeIf { eatery ->
-                eatery.id == eateryId
-            }
-        }
+        // TODO: Fix favoriting flow.
     }
 
     fun getNotificationFlowCompleted() = runBlocking {
