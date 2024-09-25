d99096b90266ecfc35c80d97d286bb7a2055a629
Olivia Jiang <jiangxolivia@gmail.com>
2024-09-24 22:12:36 -0400

    both row and icon button clickable
    in compare menus bot sheet

--- app/src/main/java/com/cornellappdev/android/eatery/ui/components/comparemenus/CompareMenusBotSheet.kt
+++ app/src/main/java/com/cornellappdev/android/eatery/ui/components/comparemenus/CompareMenusBotSheet.kt
@@ -2,6 +2,7 @@ package com.cornellappdev.android.eatery.ui.components.comparemenus
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -121,8 +122,15 @@ fun CompareMenusBotSheet(
                     LazyColumn {
                         items(eateries) { eatery ->
                             Row(
-                                modifier = Modifier.fillMaxWidth(),
-                                verticalAlignment = Alignment.CenterVertically
+                                modifier = Modifier.fillMaxWidth().clickable {
+                                    if (selectedEateries.contains(eatery)) {
+                                        homeViewModel.removeSelected(eatery)
+                                    } else {
+                                        homeViewModel.addSelected(eatery)
+                                    }
+                                },
+                                verticalAlignment = Alignment.CenterVertically,
+
                             ) {
                                 IconButton(
                                     onClick = {
