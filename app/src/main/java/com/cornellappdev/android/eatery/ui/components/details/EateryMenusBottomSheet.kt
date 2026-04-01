18b48f84a27e3bc877cb768cfc93319c738f6878
Caleb Shim <calebshim248@gmail.com>
2026-04-01 15:23:33 -0400

    Replace deprecated methods

--- app/src/main/java/com/cornellappdev/android/eatery/ui/components/details/EateryMenusBottomSheet.kt
+++ app/src/main/java/com/cornellappdev/android/eatery/ui/components/details/EateryMenusBottomSheet.kt
@@ -2,6 +2,7 @@ package com.cornellappdev.android.eatery.ui.components.details
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -12,7 +13,6 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.text.ClickableText
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Check
 import androidx.compose.material.icons.filled.Close
@@ -31,7 +31,6 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
@@ -237,20 +236,22 @@ fun EateryMenusBottomSheet(
                         color = Color.White
                     )
                 }
-                ClickableText(
-                    modifier = Modifier.padding(top = 12.dp),
-                    text = AnnotatedString("Reset"),
+                Text(
+                    modifier = Modifier
+                        .padding(top = 12.dp)
+                        .clickable {
+                            selectedDay = weekDayIndex
+                            onResetClick()
+                            onDismiss()
+                        },
+                    text = "Reset",
                     style = TextStyle(
                         fontSize = 14.sp,
                         lineHeight = 17.5.sp,
                         fontWeight = FontWeight(600),
                         color = Color(0xFF050505)
-                    ),
-                    onClick = {
-                        selectedDay = weekDayIndex
-                        onResetClick()
-                        onDismiss()
-                    })
+                    )
+                )
             }
         }
     }
