16475da16da9028525120e086c5afc1cf6317258
Caleb Shim <calebshim248@gmail.com>
2026-04-18 21:44:31 -0400

    Remove commented out code

--- app/src/main/java/com/cornellappdev/android/eatery/ui/theme/Theme.kt
+++ app/src/main/java/com/cornellappdev/android/eatery/ui/theme/Theme.kt
@@ -61,7 +61,6 @@ fun EateryBlueTheme(
 
     // For bottom sheet background colors
     val colorScheme = baseColorScheme.copy(
-//        surface = Color.White,
         surfaceContainerLowest = Color.White,
         surfaceContainerLow = Color.White,
         surfaceContainer = Color.White,
